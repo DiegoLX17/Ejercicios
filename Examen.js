78bdff1836bd3a2f23443a55c9b75badea2b8e28
Diego Luna <diegoluna390526@gmail.com>
2022-10-09 23:20:10 -0500

    commit

---
+++ Examen.js
@@ -0,0 +1,95 @@
+//primer ejercicio
+const character = 1  //ingresa aqui tu caracter
+// console.log('Ingresa tu caracter arriba en la variable','tu variable es de tipo:',typeof(character))
+// const typeofcharacter = typeof(characterfive)
+if(typeof(character) == 'number'){
+    console.log('Es una cadena de digitos')
+}
+else if (typeof(character) == 'string'){
+    console.log('Es una cadena de texto')
+}
+else(console.log('No es ninguno de los 2'))
+//
+
+// function myGeeks() {
+
+//     var str = 'jhkj7682834'
+//     var matches = str.match(/(\d+)/);
+      
+//     if (matches) {
+//         matches[0]
+//     }
+
+
+//segundo ejercicio
+const text = 'Hola mundo que tal'
+const initial = text.substring(5,10)
+var verify = text.split('mundo')
+console.log(initial,...verify)
+
+
+
+
+
+//tercer ejecicio
+const separar = 'Hola mundo prueba mundo'
+const separado = separar.split(' ')
+console.log(separado)
+// const contador = separado.find('mundo')
+//console.log(contador)
+
+
+
+
+
+
+//cuarto ejercicio
+let i
+let j
+
+console.log('Tus numeros pares son: ')
+for (i=2;i<50;i=i+2){
+    console.log(i)
+}
+console.log('Tus numeros impares son: ')
+for (i=1;i<50;i=i+2){
+    console.log(i)
+}
+
+
+
+
+
+
+//quinto ejercicio
+const characterfive = []
+if(typeof(characterfive) == 'number'){
+    console.log('Es numero')
+}
+else if (typeof(characterfive) == 'string'){
+    console.log('Es texto')
+}
+else(console.log('No es ninguno de los 2'))
+// console.log(typeofcharacter)
+
+
+// const characterfived = 'Hola'
+// const cortado = characterfived.substring(0,1)
+// console.log(cortado)
+// Mayuscula = 'H'
+// console.log(cortado == Mayuscula)
+
+// if(typeof(characterfived) == 'number'){
+//     console.log('Es numero')
+// }
+// else if (typeof(characterfived) == 'string' && cortado == Mayuscula){
+//     console.log('Es texto y empieza con Mayuscula')
+// }
+// else(console.log('No es ninguno de los 2'))
+// // console.log(typeofcharacter)
+
+//sexto ejercicio
+
+//septimo ejercicio
+
+//octavo ejercicio
